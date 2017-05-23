a5be0f6ef559e0c34acc9b84830e7e536505171f
Sviatoslav <newmedia27@gmail.com>
2017-05-23 14:30:20 +0300

    second

--- frontend/components/menu_tpl/selectProduct.php
+++ frontend/components/menu_tpl/selectProduct.php
@@ -2,7 +2,7 @@
 	value="<?= $category['id'] ?>"
 	<?php if ($category['id'] == $this->model->category_id) echo 'selected' ?>
 
-		
+	
 ><?= $tab . $category['name'] ?>
 </option>
 <?php if (isset($category['child'])): ?>
